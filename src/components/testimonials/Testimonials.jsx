5b1758b74169be41094ffb1f234b946a3ad09729
Wes Howard <BigRonald42@hotmail.com>
2021-11-23 13:31:40 -0700

    added components to main

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,9 @@
-import "./testimonals.scss"
+import "./testimonials.scss"
 
 export default function Testimonials() {
     return (
         <div className ="testimonials">
-            
+            tyuiyui
         </div>
     )
 }
