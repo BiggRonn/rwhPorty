3ab4f786c7f3c5d65405c5d2054669c34b18c5a8
Wes Howard <BigRonald42@hotmail.com>
2021-11-23 14:48:08 -0700

    scrollbar changes, adding global scss

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function intro() {
     return (
-        <div className="intro">
+        <div className="intro" id = "intro">
         
         </div>
     )
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,6 +3,15 @@ import "./topbar.scss"
 export default function topbar() {
     return (
         <div className= "topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro">THIS IS INTRO </a>
+                </div>
+
+                <div className="right">
+
+                </div>
+            </div>
             
         </div>
     )
