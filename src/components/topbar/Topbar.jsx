3165ebfef2ff6fa015252632412f418ed8237e65
Wes Howard <BigRonald42@hotmail.com>
2021-11-27 12:25:01 -0700

    adding elements in header, logos, links

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,18 +1,29 @@
 import "./topbar.scss"
+//import ComputerIcon from '@mui/icons-material/Computer';
 
 export default function topbar() {
     return (
         <div className= "topbar">
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro">THIS IS INTRO </a>
+                    <a href="#intro" className="logo">Ronald Wes Howard II </a>
+                    <div className="itemContainer">
+                        <a href="https://github.com/BiggRonn" target="_blank" rel="noreferrer" class='fa fa-github'>Github</a>
+                        
+                    </div>
+                    <div className="itemContainer">
+                        <a href="https://www.linkedin.com/in/ronaldweshoward/" target="_blank" rel="noreferrer" class='fa fa-linkedin-square'>LinkedIn</a>
+                    </div>
                 </div>
-
+               
                 <div className="right">
+                    <a href="#contact">RIGHTRIGHT</a>
 
                 </div>
             </div>
             
         </div>
+        
     )
 }
+
