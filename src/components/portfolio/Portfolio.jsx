8be3934658649bf14f68d582117ced7207d02afa
Wes Howard <BigRonald42@hotmail.com>
2021-11-28 15:57:38 -0700

    changing color scheme, start portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-           
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Featured</li>
+                <li>React</li>
+                <li>Web Apps</li>
+                <li>Backend Projects</li>
+                <li>Early Projects</li>
+            </ul>
+           <div className="container">
+               <div className="item">
+                   <img src="" alt="" />
+                   <h3>Dude, Where's My Charger?</h3>
+               </div>
+           </div>
         </div>
     )
 }
