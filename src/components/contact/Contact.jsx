d5314d8f08f0ca31d2508caab9abfc5259fca98c
Wes Howard <BigRonald42@hotmail.com>
2021-11-28 22:27:02 -0700

    Contact section added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,6 +3,18 @@ import "./contact.scss"
 export default function Contact() {
     return (
         <div className = "contact" id = "contact">
+            <div className="left">
+                <img src="assets/techBG.jpg" alt="" />
+            </div>
+
+            <div className="right">
+                <h2>Contact</h2>
+                <form action="">
+                    <input type="text" placeholder="Email" />
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
          
         </div>
     )
