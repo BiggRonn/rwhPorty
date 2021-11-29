061b4073fd57a20784ad4c2857ed52ff6dfd250f
Wes Howard <BigRonald42@hotmail.com>
2021-11-28 20:55:50 -0700

    portfolio page done, arrays added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,38 +1,73 @@
 import "./portfolio.scss"
+import PortfolioList from "../portfolioList/PortfolioList"
+import { useEffect, useState } from "react"
+import {
+    featuredPortfolio,
+    frontEndPortfolio,
+    backEndPortfolio,
+    miscPortfolio
+} from "../../data.js"
+
 
 export default function Portfolio() {
+    const [selected, setSelected] = useState("featured")
+    const [data, setData] = useState([])
+    const list = [
+        {
+            id: "featured",
+            title: "Featured"
+        },
+        {
+            id: "frontend",
+            title: "FrontEnd"
+        },
+        {
+            id: "backend",
+            title: "Backend"
+        },
+        {
+            id: "miscProjects",
+            title: "Misc. Projects"
+        },
+    ]
+
+    useEffect(() => {
+
+        switch (selected) {
+            case "featured": setData(featuredPortfolio);
+                break;
+            case "frontend": setData(frontEndPortfolio);
+                break;
+            case "backend": setData(backEndPortfolio);
+                break;
+            case "miscProjects": setData(miscPortfolio);
+                break;
+            default: setData(featuredPortfolio);
+        }
+
+    }, [selected])
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <ul>
-                <li className= "active">Featured</li>
-                <li>React</li>
-                <li>Web Apps</li>
-                <li>Backend Projects</li>
-                <li>Early Projects</li>
+                {list.map((item) => (
+                    <PortfolioList
+                        title={item.title}
+                        id={item.id}
+                        active={selected === item.id}
+                        setSelected={setSelected}
+                    />
+                ))}
             </ul>
-           <div className="container">
-               <div className="item">
-                   <img src="assets\Professional Picture.jpg" alt="" />
-                   <h3>Dude, Where's My Charger?</h3>
-               </div>
-               <div className="item">
-                   <img src="assets\Professional Picture.jpg" alt="" />
-                   <h3>Dude, Where's My Charger?</h3>
-               </div>
-               <div className="item">
-                   <img src="assets\Professional Picture.jpg" alt="" />
-                   <h3>Dude, Where's My Charger?</h3>
-               </div>
-               <div className="item">
-                   <img src="assets\Professional Picture.jpg" alt="" />
-                   <h3>Dude, Where's My Charger?</h3>
-               </div>
-               <div className="item">
-                   <img src="assets\Professional Picture.jpg" alt="" />
-                   <h3>Dude, Where's My Charger?</h3>
-               </div>
-           </div>
+            <div className="container">
+                {data.map(data=>(
+                <div className="item">
+                    <img src={data.img} alt="" />
+                    <h3>{data.title}</h3>
+                </div>
+
+                ))}
+            </div>
         </div>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: "Dude, Where's My Charger?",
+        img: "../assets/EVcharger.jpg"
+
+    }, {
+        id: 2,
+        title: "Weather App",
+        img: "public\assets\EVcharger.jpg"
+    },
+    {
+        id: 3,
+        title: "Employee Backend",
+        img: "public\assets\EVcharger.jpg"
+    }
+    
+]
+
+export const frontEndPortfolio = [
+    {
+        id: 1,
+        title: "FrontENDER",
+        img: "public\assets\EVcharger.jpg"
+
+    },
+    {
+        id: 2,
+        title: "222",
+        img: "public\assets\EVcharger.jpg"
+
+    }
+]
+
+export const backEndPortfolio = [
+    {
+        id: 1,
+        title: "BACKIEbackback",
+        img: "public\assets\EVcharger.jpg"
+
+    },
+    {
+        id: 2,
+        title: "333",
+        img: "public\assets\EVcharger.jpg"
+
+    },
+    {
+        id: 3,
+        title: "Employee Backend",
+        img: "public\assets\EVcharger.jpg"
+
+    },
+    {
+        id: 4,
+        title: "Dude, Where's My Charger?",
+        img: "public\assets\EVcharger.jpg"
+
+    }
+]
+export const miscPortfolio = [
+    {
+        id: 1,
+        title: "Miscellaneous Spelling",
+        img: "../assets/EVcharger.jpg"
+
+    }, {
+        id: 2,
+        title: "4444",
+        img: "public\assets\EVcharger.jpg"
+
+    },
+    {
+        id: 3,
+        title: "Employee Backend",
+        img: "public\assets\EVcharger.jpg"
+
+    }
+]
